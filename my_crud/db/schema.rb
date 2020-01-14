759826007e49930cc607c8e2d95d447322db10c3
Cristian Acuña <cas@codigonet.net>
2020-01-14 01:12:33 -0300

    update: rails version and dependencies. fix: explicit Ruby version to use

--- my_crud/db/schema.rb
+++ my_crud/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180911204908) do
+ActiveRecord::Schema.define(version: 2018_09_11_204908) do
 
   create_table "areas", force: :cascade do |t|
     t.string "name"
